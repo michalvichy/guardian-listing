fea7a551549d73661430e70350c92892f9461fe3
Michał <mchlskora@gmail.com>
2019-04-16 20:45:21 +0200

    Routing removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Route, Router, Switch } from 'react-router';
-import { paths } from './config/routing';
-import { createBrowserHistory } from 'history';
-import { HomePage } from './components/HomePage';
 
-const history = createBrowserHistory();
+import { HomePage } from './components/HomePage';
 
 export const App = () => {
   return (
     <div className="container">
-      <Router history={history}>
-        <Switch>
-          <Route exact path={paths.root} component={HomePage} />
-
-          <Route exact path={paths.article} render={() => <h1>Article</h1>} />
-
-          <Route render={() => <h1>Not found</h1>} />
-        </Switch>
-      </Router>
+      <HomePage />>
     </div>
   );
 };
--- src/helpers/toPath.ts
+++
@@ -1,5 +0,0 @@
-import pathToRegexp from 'path-to-regexp';
-
-export function toPath<T extends object>(path: string, data: T): string {
-  return pathToRegexp.compile(path)(data);
-}
